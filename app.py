110835bb36ff79d8ef59ce0db0493d41cdaf2b22
minjie <minjilei@umich.edu>
2020-06-08 16:35:05 -0400

    implemented list endpoints

--- app.py
+++ app.py
@@ -107,27 +107,23 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+  data = []
+  cities = Venue.query.distinct('city').all()
+  for city in cities:
+    data_entry = {}
+    data_entry['city'] = city.city
+    data_entry['state'] = city.state
+    venues = []
+    for venue in Venue.query.filter_by(city=city.city).order_by('id').all():  
+      venues_entry = {}
+      venues_entry['id'] = venue.id
+      venues_entry['name'] = venue.name
+      num_upcoming_shows = Show.query.filter(Show.venue_id==venue.id, Show.start_time>datetime.today()).count()
+      venues_entry['num_upcoming_shows'] = num_upcoming_shows
+      venues.append(venues_entry.copy())
+    data_entry['venues'] = venues
+    data.append(data_entry.copy())
+
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
@@ -293,16 +289,12 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data=[]
+  for artist in Artist.query.order_by('id').all():
+    data_entry={}
+    data_entry['id'] = artist.id
+    data_entry['name'] = artist.name
+    data.append(data_entry.copy())
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -510,42 +502,16 @@ def shows():
   # displays list of shows at /shows
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+  data=[]
+  for show in Show.query.order_by('start_time').all():
+    data_entry = {}
+    data_entry['venue_id'] = show.venue_id
+    data_entry['venue_name'] = Venue.query.filter_by(id=show.venue_id).one().name
+    data_entry['artist_id'] = show.artist_id
+    data_entry['artist_name'] = Artist.query.filter_by(id=show.artist_id).one().name
+    data_entry['artist_image_link'] = Artist.query.filter_by(id=show.artist_id).one().image_link
+    data_entry['start_time'] = show.start_time.strftime("%Y-%m-%dT%H:%M:%f")
+    data.append(data_entry.copy())
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
